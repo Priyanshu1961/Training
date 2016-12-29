13cd272483f7d7f235e4756963c80319428f91cb
Priyanshu1961 <priyanshu.a@reportgarden.com>
2016-12-29 15:21:27 +0530

    ProgressBar Assignment

---
+++ progressbar/main.js
@@ -0,0 +1,23 @@
+var count = 0;
+var button = document.getElementById("countButton");
+var display = document.getElementById("displayCount");
+
+button.onclick = function(){
+  count++;
+  display.innerHTML = count;
+}
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 0;
+  var id = setInterval(frame, 3000);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      console.log(id)
+      width++;
+      elem.style.width = width + '%';
+      document.getElementById("label").innerHTML = width * 1  + '%';
+    }
+  }
+}
